f960d144ffcc0a1ed687dc2e1ff64028733b50b1
hectornunfer <62731025+hectornunfer@users.noreply.github.com>
2023-10-30 01:29:35 +0100

    [FIX] SQLi, ProductRepository.java arreglado.

--- store-app/src/main/java/es/storeapp/business/repositories/ProductRepository.java
+++ store-app/src/main/java/es/storeapp/business/repositories/ProductRepository.java
@@ -10,12 +10,13 @@ import org.springframework.stereotype.Repository;
 public class ProductRepository extends AbstractRepository<Product> {
     
     private static final String FIND_BY_CATEGORY_QUERY = 
-            "SELECT p FROM Product p WHERE p.category.name = ''{0}'' ORDER BY p.{1}";
+            "SELECT p FROM Product p WHERE p.category.name = :category ORDER BY p.";
     
     @SuppressWarnings("unchecked")
     public List<Product> findByCategory(String category, String orderColumn) {
-        Query query = entityManager.createQuery(MessageFormat.format(FIND_BY_CATEGORY_QUERY, 
-                category, orderColumn));
+        String queryString = FIND_BY_CATEGORY_QUERY + orderColumn;
+        Query query = entityManager.createQuery(queryString);
+        query.setParameter("category", category);
         return (List<Product>) query.getResultList();
     }
     
